852aa7e6a254fcfac65e16f6ce13bc1e3d23f85f
Your GitHub Username <sn11080406@gmail.com>
2026-07-02 14:42:27 +0530

    fix: Update base path to match repo name computer_product

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/computer_system/',
+  base: '/computer_product/',
   build: {
     outDir: '../docs',
     emptyOutDir: true
